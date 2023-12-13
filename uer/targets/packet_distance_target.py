0043f6296b02cbd0383f92be19f060d98cc4a877
HungnotDumb <hungnoyop@proton.me>
2023-12-13 09:10:17 +0000

    Adding coverage badge

---
+++ uer/targets/packet_distance_target.py
@@ -0,0 +1,12 @@
+# -*- encoding:utf-8 -*-
+import math
+import torch
+import torch.nn as nn
+from uer.layers.layer_norm import LayerNorm
+from uer.utils.act_fun import gelu
+
+
+class NspTarget(nn.Module):
+    """
+    """
+    def
